08bd8d241772b221af47c1c6e3f548904c6765b7
sakaiyuji3862 <sakaiya0526@icloud.com>
2020-12-08 14:28:05 +0900

    Issue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
+    get_week
     @plan = Plan.new
   end
 
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def getWeek
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
@@ -26,7 +26,8 @@ class CalendarsController < ApplicationController
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
-
+    # whereメソッドとは、テーブル内の条件に一致したレコードを配列の形で取得することができるメソッド
+    # 今日の日付と、そのほかの
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
